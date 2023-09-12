b244cbd2dc7e0bf5d3bee651372430a200990de8
Tatwomey <75634518+Tatwomey@users.noreply.github.com>
2023-09-12 12:16:19 -0400

    improve styles, add images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Expletus_Sans } from "next/font/google";
+
+const expletusSans = Expletus_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component
+      {...pageProps}
+      className={`${expletusSans.className} font-sans`}
+    />
+  );
 }
